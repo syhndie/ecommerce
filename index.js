86b4c0d4831c9b63c55bccc57e5306d843e1f47b
Cindy <syhndie2@gmail.com>
2021-03-22 11:13:50 -0500

    added get and post requests to root page, using bodyparser

--- index.js
+++ index.js
@@ -4,13 +4,31 @@ const express = require('express');
 
 //app is an object that describes all the things our web server can do
 const app = express();
+//this tells express to apply bodyparser middleware to all route handlers
+app.use(express.urlencoded({ extended: true }));
 
 //route handler for get requests
 //req stands for request
 //res stands for response
-//first parameter defines the kind of request - / means root route
+//first parameter defines the path for the http request
 app.get('/', (req, res) => {
-    res.send('hi there');
+    res.send(`
+        <div>
+            <form method="POST">
+                <input name="email" placeholder="email" />
+                <input name="password" placeholder="password" />
+                <input name="passwordConfirmation" placeholder="password confirmation" />
+                <button>Sign Up</button>
+            </form>
+        </div>
+    `);
+});
+
+//in the course SG uses bodyparser
+//this is deprecated and bodyparser is now included directly in express
+app.post('/', (req, res) => {
+    console.log(req.body);
+    res.send('account created');
 });
 
 //add port listener
