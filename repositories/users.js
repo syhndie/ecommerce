078408bc88d072774180cc2eac10b704e7559440
Cindy <syhndie2@gmail.com>
2021-03-25 10:42:04 -0500

    added update method to users.js

--- repositories/users.js
+++ repositories/users.js
@@ -61,11 +61,23 @@ class UsersRepository {
         const filteredRecords = records.filter(record => record.id !== id);
         await this.writeAll(filteredRecords);
     }
+
+    async update(id, attrs) {
+        const records = await this.getAll();
+        const record = records.find(record => record.id === id);
+
+        if (!record) {
+            throw new Error(`Record with id ${id} not found.`);
+        }
+
+        Object.assign(record, attrs);
+        await this.writeAll(records);
+    }
 }
 const test = async () => {
     const repo = new UsersRepository('users.json');
 
-    await repo.delete('fa898f2c');
+    await repo.update('2caed919sss', {password: 'newpassword'});
 };
 
 test();
