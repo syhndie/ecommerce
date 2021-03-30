03e2f55af13170cca387a7e3773e85b30d221902
Cindy <syhndie2@gmail.com>
2021-03-30 16:03:32 -0500

    added signin and signout pages

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.use(cookieSession({
 //req stands for request
 //res stands for response
 //first parameter defines the path for the http request
-app.get('/', (req, res) => {
+app.get('/signup', (req, res) => {
     res.send(`
         <div>
         Your id is: ${req.session.userId}
@@ -36,7 +36,7 @@ app.get('/', (req, res) => {
 
 //in the course SG uses bodyparser
 //this is deprecated and bodyparser is now included directly in express
-app.post('/', async (req, res) => {
+app.post('/signup', async (req, res) => {
     const { email, password, passwordConfirmation } = req.body;
 
     const existingUser = await usersRepo.getOneBy({ email });
@@ -54,6 +54,42 @@ app.post('/', async (req, res) => {
     //define userId property in the user's cookie
     req.session.userId = user.id;
     res.send('account created');
+});
+
+app.get('/signout', (req, res) => {
+    //remove cookie session by setting to null
+    req.session = null;
+    res.send('You are logged out');
+});
+
+app.get('/signin', (req, res) => {
+    res.send(`
+    <div>
+        <form method="POST">
+            <input name="email" placeholder="email" />
+            <input name="password" placeholder="password" />
+            <button>Sign In</button>
+        </form>
+    </div>
+    `);
+});
+
+app.post('/signin', async (req, res) => {
+    const { email, password } = req.body;
+
+    const user = await usersRepo.getOneBy({ email });
+
+    if (!user) {
+        return res.send('Email not found');
+    }
+
+    if (user.password !== password) {
+        return res.send('Invalid password');
+    }
+
+    req.session.userId = user.id;
+
+    res.send(`${user.email} is signed in.`);
 });
 
 //add port listener
