34912985925c31f9154a3afb0e1c66724ebf02f5
Cindy <syhndie2@gmail.com>
2021-04-01 16:13:51 -0500

    on user signup password is salted and hashed before storing

--- repositories/users.js
+++ repositories/users.js
@@ -1,6 +1,9 @@
 //fs is library that inteacts with the file system
 const fs = require('fs');
 const crypto =  require('crypto');
+const util = require('util');
+
+const scrypt= util.promisify(crypto.scrypt);
 
 class UsersRepository {
     constructor(filename) {
@@ -31,14 +34,24 @@ class UsersRepository {
         );
     }
 
+    //we are assuming that the attrs object always has an 
+    //email property and a password property, both are strings
     async create(attrs) {
         attrs.id = this.randomId();
+        
+        const salt = crypto.randomBytes(8).toString('hex');
+        const buf = await scrypt(attrs.password, salt, 64);
+
         const records = await this.getAll();
-        records.push(attrs);
+        const record = {
+            ...attrs,
+            password: `${buf.toString('hex')}.${salt}`
+        };
+        records.push(record);
 
         await this.writeAll(records);
 
-        return attrs;
+        return record;
     }
     
     async writeAll(records) {
