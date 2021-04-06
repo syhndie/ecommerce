13a332ff5959a6b795e6e40fe147af571dbb08c1
Cindy <syhndie2@gmail.com>
2021-04-06 15:49:08 -0500

    created views for signin and signup pages

---
+++ views/admin/auth/signin.js
@@ -0,0 +1,12 @@
+module.exports = ({ req }) => {
+    return `
+        <div>
+            Your id is: ${req.session.userId}
+            <form method="POST">
+                <input name="email" placeholder="email" />
+                <input name="password" placeholder="password" />
+                <button>Sign In</button>
+            </form>
+        </div>
+    `
+};
