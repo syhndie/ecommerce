5e51ade5726451b0541731db27930d04c57861fa
Cindy <syhndie2@gmail.com>
2021-04-07 14:41:16 -0500

    signin.js refactor to use layout

--- views/admin/auth/signin.js
+++ views/admin/auth/signin.js
@@ -2,7 +2,7 @@ const layout = require('../layout');
 
 module.exports = ({ req }) => {
     return layout({ 
-        content:`
+        content: `
             <div>
                 Your id is: ${req.session.userId}
                 <form method="POST">
