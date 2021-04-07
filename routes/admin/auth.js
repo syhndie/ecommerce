e7fa27312d42545666860396d073df61f25676a3
Cindy <syhndie2@gmail.com>
2021-04-07 15:46:11 -0500

    using express-validator for signup form

--- routes/admin/auth.js
+++ routes/admin/auth.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { check, validationResult } = require('express-validator');
 const usersRepo = require('../../repositories/users');
 const signupTemplate = require('../../views/admin/auth/signup');
 const signinTemplate = require('../../views/admin/auth/signin');
@@ -16,25 +17,48 @@ router.get('/signup', (req, res) => {
 
 //in the course SG uses bodyparser
 //this is deprecated and bodyparser is now included directly in express
-router.post('/signup', async (req, res) => {
-    const { email, password, passwordConfirmation } = req.body;
+router.post(
+    '/signup', 
+    [
+        check('email')
+            .trim()
+            .normalizeEmail()
+            .isEmail()
+            .withMessage('Must be a valid email')
+            .custom(async (email) => {
+                const existingUser = await usersRepo.getOneBy({ email });
+                if (existingUser) {
+                    throw new Error('Email in use');
+        }
+            }),
+        check('password')
+            .trim()
+            .isLength({ min: 4, max: 20 })
+            .withMessage('Must be betwwen 4 and 20 characters'),
+        check('passwordConfirmation')
+            .trim()
+            .isLength({ min: 4, max: 20 })
+            .withMessage('Must be betwwen 4 and 20 characters')
+            .custom((passwordConfirmation, { req }) => {
+                if (passwordConfirmation != req.body.password) {
+                    throw new Error('Passwords must match');
+                }
+            })
+    ], 
+    async (req, res) => {
+        const errors = validationResult(req);
+        console.log(errors);
 
-    const existingUser = await usersRepo.getOneBy({ email });
-    if (existingUser) {
-        return res.send('Email in use');
+        const { email, password, passwordConfirmation } = req.body;
+
+        //create returns user all user information from the repo
+        const user = await usersRepo.create({ email, password });
+
+        //define userId property in the user's cookie
+        req.session.userId = user.id;
+        res.send('account created');
     }
-
-    if (password !== passwordConfirmation) {
-        return res.send('Passwords must match');
-    }
-
-    //create returns user all user information from the repo
-    const user = await usersRepo.create({ email, password });
-
-    //define userId property in the user's cookie
-    req.session.userId = user.id;
-    res.send('account created');
-});
+);
 
 router.get('/signout', (req, res) => {
     //remove cookie session by setting to null
