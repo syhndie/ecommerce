0fdd06663decf39eaddd37c1b923823102da0f80
Cindy <syhndie2@gmail.com>
2021-04-20 15:47:08 -0500

    new products form allows image upload and saving as string in product repo

--- routes/admin/products.js
+++ routes/admin/products.js
@@ -1,10 +1,18 @@
 const express = require('express');
 const { validationResult } = require('express-validator');
+//multer is middleware used to handle multi-part form data
+//we need this to allow the user to upload an image
+const multer = require('multer');
+
 const productsRepo = require('../../repositories/products');
 const productsNewTemplate = require('../../views/admin/products/new');
 const { requireTitle, requirePrice } = require('./validators');
 
 const router = express.Router();
+
+// memory storage stores the files in memory as Buffer objects
+//this wouldn't work with large files or many files at once
+const upload = multer({ storage: multer.memoryStorage() });
 
 //Route handlers
 
@@ -22,11 +30,29 @@ router.get('/admin/products/new', (req, res) => {
 
 //3
 //post new product form
-router.post('/admin/products/new', [requireTitle, requirePrice], (req, res) => {
-    const errors = validationResult(req);
-    console.log(errors);
-    res.send('submitted');
-});
+router.post(
+    '/admin/products/new', 
+    //the upload accepts a single file called image
+    //this must come before the validation step
+    //because there is not req.bod until this has happened
+    upload.single('image'),
+    [requireTitle, requirePrice], 
+    async (req, res) => {
+        const errors = validationResult(req);
+
+        if (!errors.isEmpty()) {
+            return res.send(productsNewTemplate({ errors }));
+        }
+
+        //multer stored the image in req.file.buffer
+        //we store it in products.json as a base64 string
+        const image = req.file.buffer.toString('base64');
+
+        const { title, price } = req.body;
+        await productsRepo.create({ title, price, image });
+
+        res.send('submitted');
+    });
 
 //4
 //get product edit/delete form
--- views/admin/products/new.js
+++ views/admin/products/new.js
@@ -3,10 +3,14 @@ const { getError } = require('../../helpers');
 
 module.exports = ({ errors }) => {
     return layout({
+        //we must specify enctype as multipart/form-data 
+        //to allow uploading image using multer middleware
         content: `
-            <form method="POST">
+            <form method="POST" enctype="multipart/form-data">
                 <input placeholder="Title" name="title" />
+                ${getError(errors, 'title')}
                 <input placeholder="Price" name="price" />
+                ${getError(errors, 'price')}
                 <input type="file" name="image" />
                 <button>Submit</button>
             </form>
