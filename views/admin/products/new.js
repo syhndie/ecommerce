b1dbcc8d258d5f6e5aab44c4233bf3ec1bbb02b4
Cindy <syhndie2@gmail.com>
2021-04-20 16:10:18 -0500

    added styling to new products view

--- views/admin/products/new.js
+++ views/admin/products/new.js
@@ -6,14 +6,32 @@ module.exports = ({ errors }) => {
         //we must specify enctype as multipart/form-data 
         //to allow uploading image using multer middleware
         content: `
+        <div class="columns is-centered">
+          <div class="column is-half">
+            <h1 class="subtitle">Create a Product</h1>
+  
             <form method="POST" enctype="multipart/form-data">
-                <input placeholder="Title" name="title" />
-                ${getError(errors, 'title')}
-                <input placeholder="Price" name="price" />
-                ${getError(errors, 'price')}
+              <div class="field">
+                <label class="label">Title</label>
+                <input class="input" placeholder="Title" name="title">
+                <p class="help is-danger">${getError(errors, 'title')}</p>
+              </div>
+              
+              <div class="field">
+                <label class="label">Price</label>
+                <input class="input" placeholder="Price" name="price">
+                <p class="help is-danger">${getError(errors, 'price')}</p>
+              </div>
+              
+              <div class="field">
+                <label class="label">Image</label>            
                 <input type="file" name="image" />
-                <button>Submit</button>
+              </div>
+              <br />
+              <button class="button is-primary">Create</button>
             </form>
-        `
+          </div>
+        </div>
+      `
     });
 };
