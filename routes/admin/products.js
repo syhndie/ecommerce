c7c6f2eec2514ce84801c648a454e621045c1486
Cindy <syhndie2@gmail.com>
2021-04-21 16:01:33 -0500

    added products listing view and basic route handling for it

--- routes/admin/products.js
+++ routes/admin/products.js
@@ -7,6 +7,7 @@ const multer = require('multer');
 const { handleErrors } = require('./middlewares');
 const productsRepo = require('../../repositories/products');
 const productsNewTemplate = require('../../views/admin/products/new');
+const productsIndexTemplate = require('../../views/admin/products/index');
 const { requireTitle, requirePrice } = require('./validators');
 
 const router = express.Router();
@@ -19,8 +20,9 @@ const upload = multer({ storage: multer.memoryStorage() });
 
 //1
 //get product listsing
-router.get('/admin/products', (req, res) => {
-
+router.get('/admin/products', async (req, res) => {
+    const products = await productsRepo.getAll();
+    res.send(productsIndexTemplate({ products }));
 });
 
 //2
