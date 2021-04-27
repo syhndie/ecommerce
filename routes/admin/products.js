be92678fece4c73a72f89f9b5986dd3267708eeb
Cindy <syhndie2@gmail.com>
2021-04-27 15:58:37 -0500

    added basic product edit functionality

--- routes/admin/products.js
+++ routes/admin/products.js
@@ -8,6 +8,7 @@ const { handleErrors, requireAuth } = require('./middlewares');
 const productsRepo = require('../../repositories/products');
 const productsNewTemplate = require('../../views/admin/products/new');
 const productsIndexTemplate = require('../../views/admin/products/index');
+const productsEditTemplate = require('../../views/admin/products/edit')
 const { requireTitle, requirePrice } = require('./validators');
 
 const router = express.Router();
@@ -45,11 +46,10 @@ router.post(
     async (req, res) => {        
         //multer stored the image in req.file.buffer
         //we store it in products.json as a base64 string
-        let image;
-        try {
+        if (req.file) {
             image = req.file.buffer.toString('base64');
-        } catch {
-            image='';
+        } else {
+            image ='';
         }
     
         const { title, price } = req.body;
@@ -60,9 +60,40 @@ router.post(
 
 //4
 //get product edit/delete form
+router.get('/admin/products/:id/edit', requireAuth, async (req, res) => {
+    const product = await productsRepo.getOne(req.params.id);
+
+    if (!product) {
+        return res.send('Product not found');
+    }
+
+    res.send(productsEditTemplate({ product }));
+});
 
 //5
 //post product edit form
+router.post(
+    '/admin/products/:id/edit', 
+    requireAuth, 
+    upload.single('image'),
+    [requireTitle, requirePrice],
+    handleErrors(productsEditTemplate),
+    async (req, res) => {
+        const changes = req.body;
+
+        if (req.file) {
+            changes.image = req.file.buffer.toString('base64');
+        }
+
+        try {
+        await productsRepo.update(req.params.id, changes)
+        } catch (err) {
+            res.send('Could not find item');
+        }
+
+        res.redirect('/admin/products');
+    }
+);
 
 //6
 //post product deletion
